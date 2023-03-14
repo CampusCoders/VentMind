0488f65b0ce277d3293670c11fe009596e5cf910
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-03-14 22:33:12 +0300

    added fragments

---
+++ app/src/main/java/com/campuscoders/ventmind/view/FeedFragment.kt
@@ -0,0 +1,30 @@
+package com.campuscoders.ventmind.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.campuscoders.ventmind.databinding.FragmentFeedBinding
+
+class FeedFragment: Fragment() {
+
+    private var _binding: FragmentFeedBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentFeedBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //logic
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
