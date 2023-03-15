f36eeaf95bee7b0733172209b4eacf7157b25666
Emirhan Tarım <emirhan.4.t.t@gmail.com>
2023-03-15 23:42:09 +0300

    added TrendFragment

---
+++ app/src/main/java/com/campuscoders/ventmind/view/TrendFragment.kt
@@ -0,0 +1,29 @@
+package com.campuscoders.ventmind.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.campuscoders.ventmind.databinding.FragmentTrendBinding
+
+class TrendFragment:Fragment(){
+    private  var _binding:FragmentTrendBinding?=null
+    private  val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        _binding = FragmentTrendBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //logic
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
