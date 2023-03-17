080b6240142df8fa6c44a03acf9193ec9018ca7f
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-03-17 18:12:25 +0300

    tested firebase

--- app/src/main/java/com/campuscoders/ventmind/MainActivity.kt
+++ app/src/main/java/com/campuscoders/ventmind/MainActivity.kt
@@ -2,6 +2,7 @@ package com.campuscoders.ventmind
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,5 +10,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myMap = HashMap<String,Any>()
+        myMap["id"] = 123123123
+        myMap["name"] = "expname"
+        myMap["age"] = 15
+
+        FirebaseFirestore.getInstance().collection("Users").add(myMap)
+            .addOnSuccessListener {
+                println("success + " + it.id)
+            }
+            .addOnFailureListener {
+                println("failure")
+                println(it.localizedMessage)
+            }
+
     }
 }
--- app/src/main/java/com/campuscoders/ventmind/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/campuscoders/ventmind/viewmodel/AuthViewModel.kt
@@ -3,6 +3,5 @@ package com.campuscoders.ventmind.viewmodel
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 
-@HiltViewModel
 class AuthViewModel: ViewModel() {
 }
