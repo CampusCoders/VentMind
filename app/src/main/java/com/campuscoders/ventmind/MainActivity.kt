d8106ad970c5248a151f7ab5375d7abbf1297fa3
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-03-17 21:18:01 +0300

    cmt

--- app/src/main/java/com/campuscoders/ventmind/MainActivity.kt
+++ app/src/main/java/com/campuscoders/ventmind/MainActivity.kt
@@ -2,7 +2,6 @@ package com.campuscoders.ventmind
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,20 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val myMap = HashMap<String,Any>()
-        myMap["id"] = 123123123
-        myMap["name"] = "expname"
-        myMap["age"] = 15
-
-        FirebaseFirestore.getInstance().collection("Users").add(myMap)
-            .addOnSuccessListener {
-                println("success + " + it.id)
-            }
-            .addOnFailureListener {
-                println("failure")
-                println(it.localizedMessage)
-            }
-
     }
 }
