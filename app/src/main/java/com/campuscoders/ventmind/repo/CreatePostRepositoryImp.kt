c202b47c2b0469e6973b121b445e91bd75ddcbe5
Okan Çezik <okacezik@gmail.com>
2023-03-23 16:13:54 +0300

    fixed repository of user

--- app/src/main/java/com/campuscoders/ventmind/repo/CreatePostRepositoryImp.kt
+++ app/src/main/java/com/campuscoders/ventmind/repo/CreatePostRepositoryImp.kt
@@ -3,6 +3,7 @@ package com.campuscoders.ventmind.repo
 import com.campuscoders.ventmind.model.PostExp
 import com.campuscoders.ventmind.model.PostFeed
 import com.campuscoders.ventmind.model.User
+import com.campuscoders.ventmind.util.FirestoreCollection
 import com.campuscoders.ventmind.util.UiState
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -12,16 +13,53 @@ class CreatePostRepositoryImp(
     val auth: FirebaseAuth
 ): CreatePostRepository {
 
-    override fun getUser(result: (UiState<User>) -> Unit) {
-        // User'dan kullanıcının verileri çekilir.
+    override fun getUser(userId:String ,result: (UiState<User>) -> Unit) {
         val userId = auth.currentUser
+        val document = database.collection(FirestoreCollection.USER).document(userId.toString())
+        document.get()
+            .addOnSuccessListener {
+                if(it.exists()){
+                    val user = it.toObject(User::class.java)
+                    result.invoke(
+                        UiState.Success(user!!)
+                    )
+                }else{
+                    result.invoke(UiState.Failure("error"))
+                }
+            }
+            .addOnFailureListener{
+                result.invoke(
+                    UiState.Failure(it.localizedMessage)
+                )
+            }
     }
 
     override fun addPostFeed(post: PostFeed, result: (UiState<String>) -> Unit) {
-        // PostFeed'e yeni bir document ekle
+       // post.post_user_id = auth.currentUser.uid
+        database.collection(FirestoreCollection.POST_FEED).add(post)
+            .addOnCompleteListener {
+                if (it.isSuccessful){
+                    result.invoke(UiState.Success("Post has been added"))
+                }else{
+                    result.invoke(UiState.Failure("Post add operation failed"))
+                }
+            }
+            .addOnFailureListener{
+                result.invoke(UiState.Failure("Post add operation failed"))
+            }
     }
 
     override fun addPostExp(post: PostExp, result: (UiState<String>) -> Unit) {
-        // PostExp'e yeni bir document ekler
+        database.collection(FirestoreCollection.POST_EXP).add(post)
+            .addOnCompleteListener {
+                if (it.isSuccessful){
+                    result.invoke(UiState.Success("Post has been added"))
+                }else{
+                    result.invoke(UiState.Failure("Post add operation failed"))
+                }
+            }
+            .addOnFailureListener{
+                result.invoke(UiState.Failure("Post add operation failed"))
+            }
     }
 }
