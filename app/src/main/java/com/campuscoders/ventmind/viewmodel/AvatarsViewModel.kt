2724c207ebfd3af9631f10893bcc423dd57fd10b
Okan Çezik <okacezik@gmail.com>
2023-03-27 10:49:48 +0300

    fixed AvatarsViewModel

--- app/src/main/java/com/campuscoders/ventmind/viewmodel/AvatarsViewModel.kt
+++ app/src/main/java/com/campuscoders/ventmind/viewmodel/AvatarsViewModel.kt
@@ -1,12 +1,40 @@
 package com.campuscoders.ventmind.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.campuscoders.ventmind.model.Avatar
+import com.campuscoders.ventmind.repo.AvatarsRepository
+import com.campuscoders.ventmind.repo.CreatePostRepositoryImp
+import com.campuscoders.ventmind.util.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class AvatarsViewModel @Inject constructor(
-
+    private val repository: AvatarsRepository
 ): ViewModel() {
+    private val _avatars = MutableLiveData<UiState<List<Avatar>>>()
+    val avatar : LiveData<UiState<List<Avatar>>>
+        get() = _avatars
+
+    private val _userScore = MutableLiveData<UiState<Int>>()
+    val userScore : LiveData<UiState<Int>>
+        get() = _userScore
+
+
+    fun getAvatarsFun(){
+        _avatars.value = UiState.Loading
+        repository.getAvatars (){
+            _avatars.value = it
+        }
+    }
+
+    fun getUserScore(){
+        _userScore.value = UiState.Loading
+        repository.getUserScore (){
+            _userScore.value = it
+        }
+    }
 
 }
