037c168b75012114895b6c574db97236f6a11936
Okan Çezik <okacezik@gmail.com>
2023-03-29 19:08:56 +0300

    fixed AvatarsFragment

--- app/src/main/java/com/campuscoders/ventmind/view/AvatarsFragment.kt
+++ app/src/main/java/com/campuscoders/ventmind/view/AvatarsFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.campuscoders.ventmind.adapter.AvatarAdapter
 import com.campuscoders.ventmind.databinding.FragmentAvatarsBinding
 import com.campuscoders.ventmind.viewmodel.AvatarsViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,6 +20,9 @@ class AvatarsFragment: Fragment() {
 
     val viewModel: AvatarsViewModel by viewModels()
 
+    private val avatarAdaper by lazy{
+        AvatarAdapter()
+    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentAvatarsBinding.inflate(inflater,container,false)
         return binding.root
@@ -26,7 +31,14 @@ class AvatarsFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // logic
+        binding.recyclerViewAvatars.adapter = avatarAdaper
+        binding.recyclerViewAvatars.layoutManager = LinearLayoutManager(requireContext())
+
+        observer()
+    }
+
+    private fun observer(){
+
     }
 
     override fun onDestroyView() {
